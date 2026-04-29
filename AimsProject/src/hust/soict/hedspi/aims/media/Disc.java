6c4c1a43b0d3570f65004852011ab31f89ffb343
LamQuangHung <hungwindy36@gmail.com>
2026-04-29 12:21:43 +0700

    readingassignment

--- AimsProject/src/hust/soict/hedspi/aims/media/Disc.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/Disc.java
@@ -5,7 +5,7 @@ public class Disc extends Media {
     private String director;
 
     public Disc(int id, String title, String category, float cost, int length, String director ){
-        super(id,title,category,cost);
+        super( id, title, category,cost);
         this.length = length;
         this.director = director;
     }
